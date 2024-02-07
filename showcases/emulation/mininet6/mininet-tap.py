4734c3d55dc0cc929d5f3419da34f7ee7d72c49e
Gyorgy Szaszko <adam.szaszko@gmail.com>
2024-02-07 10:04:04 +0100

    x

--- showcases/emulation/mininet6/mininet-tap.py
+++ showcases/emulation/mininet6/mininet-tap.py
@@ -48,85 +48,26 @@ net = Mininet( topo=Topo(), waitConnected=True)
 r1 = net.addHost('r1', cls=LinuxRouter)
 r1.cmd("sysctl net.ipv4.conf.all.proxy_arp=1")
 
-# host1 = net.hosts[0]
-# host2 = net.hosts[1]
-
 host1 = net.addHost('h1', defaultRoute='via 192.168.1.1')
 host2 = net.addHost('h2', defaultRoute='via 192.168.2.1')
 
 net.addLink('h1', 'r1', intfName2='r1-eth0', params2={ 'ip' : '192.168.1.1/24' })
 net.addLink('h2', 'r1', intfName2='r1-eth1', params2={ 'ip' : '192.168.2.1/24' })
 
-# net.staticArp()
-
 host1.setIP(intf='h1-eth0', ip='192.168.1.2')
 host2.setIP(intf='h2-eth0', ip='192.168.2.2')
 
-# r1.setIP(intf='r1-eth0', ip='192.168.1.1')
-# r1.setIP(intf='r1-eth1', ip='192.168.2.1')
-# r1.setIP(intf='r1-eth1', ip='10.0.0.3')
-
-# # create tap interfaces
-# subprocess.run(['bash', '-c', 'sudo tunctl -t tapa -u $SAVED_USER -g $SAVED_GROUP && sudo ip link set dev tapa up'])
-
+# create veth interfaces
 subprocess.run(['bash', '-c', 'ip link add veth1 type veth peer name veth2 && ip addr add 10.0.3.1 dev veth1 && ip link set veth1 up'])
 
-# info( '*** Checking', 'tapa', '\n' )
-# checkIntf( 'tapa' )
+info( '*** Checking', 'veth2', '\n' )
+checkIntf( 'veth2' )
 
 info( '*** Adding hardware interface', 'veth2', 'to host',
       r1.name, '\n' )
 _intf = Intf( 'veth2', node=r1, ip='192.168.3.1/24' )
 
-# info( '*** Note: you may need to reconfigure the interfaces for '
-#       'the Mininet hosts:\n', net.hosts, '\n' )
-
-# pid1 = net.hosts[0].pid
-# pid2 = net.hosts[1].pid
-# pid = r1.pid
-# cmd = f"sudo ip netns attach r1 {pid}"
-# print("cmd", cmd)
-# subprocess.run(['bash', '-c', str(cmd)])
-
-# cmd1 = """sudo ip netns attach host1 """ + str(pid1)
-# cmd2 = """sudo ip netns attach host2 """ + str(pid2)
-
-# subprocess.run(['bash', '-c', str(cmd1)])
-# subprocess.run(['bash', '-c', str(cmd2)])
-
 net.start()
-
-# uncomment to run wireshark on host
-# host1.cmd("wireshark -i tapa -k &")
-#host2.cmd("wireshark -i tapb -k &")
-
-# r2.cmd("ip route del 10.0.0.0/24")
-# r2.cmd("ip route del 10.0.0.0/24")
-# r2.cmd("ip route del 10.0.0.0/30")
-# r2.cmd("ip route del 10.0.0.0/8")
-# r2.cmd("ip route del 10.0.1.0/28")
-# r2.cmd("ip route del 10.0.1.0/28")
-# r2.cmd("ip route add 10.0.0.6 dev tapb")
-# r2.cmd("ip route add 10.0.0.2 dev tapa")
-# r2.cmd("ip route add 10.0.1.0/24 dev r2-eth0")
-
-# r2.cmd("ip route add 10.0.1.1 dev tapb")
-# r2.cmd("ip route add 10.0.1.2 dev tapa")
-# r2.cmd("ip route add 10.0.0.0/24 dev r2-eth0")
-
-# r1.cmd("ip route del 192.0.0.0/8")
-# r1.cmd("ip route del 192.0.0.0/8")
-# r1.cmd("ip route del 192.168.3.0/24")
-
-# r1.cmd("ip route add 192.168.1.0/24 via 192.168.1.1 dev r1-eth0")
-# r1.cmd("ip route add 192.168.2.0/24 via 192.168.2.1 dev r1-eth1")
-# r1.cmd("ip route add default dev tapa")
-# r1.cmd("ip route add 10.0.1.0/24 dev tapa")
-# # r1.cmd("ip route add 10.0.0.0/24 dev r1-eth0")
-# r1.cmd("ip route add 10.0.0.6 dev r1-eth1")
-# r1.cmd("ip route add 10.0.0.5 dev r1-eth0")
-# r1.cmd("ip route add 10.0.1.0 dev r1-eth0")
-# r1.cmd("ip route add 10.0.0.0 dev r1-eth0 metric 100")
 
 r1.cmd("ip route add default via 192.168.3.1 dev veth2")
 host1.cmd("ip route add default dev h1-eth0")
@@ -140,12 +81,6 @@ saved_env["PATH"] = saved_path
 
 # start inet
 subprocess.run(['inet -f omnetpp.ini -c General &'], user=user, group=group, env=saved_env, shell=True)
-
-# host1.cmd("ip route add 192.168.3.20 dev tapa")
-# host2.cmd("ip route add 192.168.2.20 dev tapb")
-
-# host1.cmd("xterm -e iperf -s &")
-# host2.cmd("xterm -e iperf -c 192.168.2.20 &")
 
 CLI( net )
 
